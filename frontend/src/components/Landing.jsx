ac0a8859a34a2ed31be26e51f015574879092e40
Zahanboi <zahansharma123@gmail.com>
2025-12-21 20:17:13 +0530

    added a button

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,6 +1,8 @@
+import Button from "../utils/button"
 
 export default function Landing() {
   return (
-    <div className="text-3xl bg-black text-white ">Landing</div>
+    <><div className="text-3xl bg-black text-white ">Landing</div>
+    <div><Button text={"Click me bro"} /></div></>
   )
 }
